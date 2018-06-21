75d95ad68449b2adfeb97bfe15d32230fd391eee
llj112293 <Midor1@outlook.com>
2018-06-21 14:54:20 +0800

    Debug

--- src/main/main.go
+++ src/main/main.go
@@ -11,6 +11,13 @@ func SayHello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello"))
 }
 
+func Options(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods","POST")
+}
+
 func main() {
 	var conf, _ = config.C.GetConfig()
 	rtr := mux.NewRouter()
@@ -23,6 +30,7 @@ func main() {
 	rtr.HandleFunc("/validate",controller.EmailValidate).Methods("POST")
 	rtr.HandleFunc("/map",controller.Mapping).Methods("POST")
 	rtr.HandleFunc("/crud",controller.InfoCRUD).Methods("POST")
+	rtr.HandleFunc("/{any}",Options).Methods("OPTIONS")
 	http.Handle("/", rtr)
 	http.ListenAndServe(conf.Server.Port, nil)
 }
